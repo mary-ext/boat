ce6c3e3daf703c304f5928fc008d006389c8348e
Mary <git@mary.my.id>
2025-02-01 07:35:13 +0700

    refactor: unused file

--- src/lib/utils/crypto.ts
+++
@@ -1,5 +0,0 @@
-export const P256_PRIVATE_PREFIX = Uint8Array.from([0x86, 0x26]);
-export const P256_PUBLIC_PREFIX = Uint8Array.from([0x80, 0x24]);
-
-export const K256_PRIVATE_PREFIX = Uint8Array.from([0x81, 0x26]);
-export const K256_PUBLIC_PREFIX = Uint8Array.from([0xe7, 0x01]);
