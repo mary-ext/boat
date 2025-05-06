fb68e4146950ab1dedbf3ac75ef7cccbf02c483b
Mary <git@mary.my.id>
2025-05-06 16:15:49 +0700

    refactor: touch up on promise queue

--- src/lib/utils/promise-queue.ts
+++ src/lib/utils/promise-queue.ts
@@ -1,5 +1,12 @@
+import Queue from '@mary/ds-queue';
+
+interface QueueTask {
+	deferred: PromiseWithResolvers<any>;
+	fn: () => any;
+}
+
 export class PromiseQueue {
-	#queue: { deferred: PromiseWithResolvers<any>; fn: () => any }[] = [];
+	#queue = new Queue<QueueTask>();
 
 	#max: number;
 	#current = 0;
@@ -11,21 +18,26 @@ export class PromiseQueue {
 	add<T>(fn: () => Promise<T>): Promise<T> {
 		const deferred = Promise.withResolvers<T>();
 
-		this.#queue.push({ deferred, fn });
+		this.#queue.enqueue({ deferred, fn });
 		this.#run();
 
 		return deferred.promise;
 	}
 
 	async flush(): Promise<void> {
-		while (this.#queue.length > 0) {
-			await Promise.all(this.#queue.map((task) => task.deferred.promise));
+		while (this.#queue.size > 0) {
+			// type assertion here because JSR omits the [Symbol.iterator] method declaration
+			await Promise.all(
+				Array.from(this.#queue as any as Iterable<QueueTask>, (task) => task.deferred.promise),
+			);
 		}
 	}
 
 	#run() {
-		if (this.#queue.length > 0 && this.#current <= this.#max) {
-			const { deferred, fn } = this.#queue.shift()!;
+		let task: QueueTask | undefined;
+
+		if (this.#current <= this.#max && (task = this.#queue.dequeue()) !== undefined) {
+			const { deferred, fn } = task;
 			this.#current++;
 
 			const promise = new Promise((r) => r(fn()));
